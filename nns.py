738fe9be087380f581e4af69899570e31acfe2bf
Harsha Navalkar <harsha.navalkar@gmail.com>
2023-04-30 15:59:56 +0530

    Changes

--- nns.py
+++ nns.py
@@ -19,21 +19,25 @@ class NewsNotification:
         whatsapp_no = df_for_each_contact['whatsapp_no'].iloc[0]
         return contact_name, email_id, whatsapp_no
     
-    def get_associated_news(category, sub_cateogry):
-        # get the information in list
-        pass
+    def get_associated_news(self, contact_id, category, sub_category, df_for_each_contact):
+        each_contact_category_list = df_for_each_contact['category'].tolist()
+        each_contact_sub_category_list = df_for_each_contact['sub_category'].tolist()
+        return each_contact_category_list, each_contact_sub_category_list
 
-    def match_news(contact_name, email_id, whatsapp_no, df_for_each_contact):
+    def match_news(self, contact_name, email_id, whatsapp_no, df_for_each_contact):
+        each_contact_category_list,each_contact_sub_category_list = self.get_associated_news(contact_name, email_id, whatsapp_no, df_for_each_contact)
         for index, row in df_for_each_contact.iterrows():
             category = row['category']
             sub_category = row['sub_category']
+            if ( category in each_contact_category_list) and (sub_category in each_contact_sub_category_list):
+                      send_email_notification(contact_name, email_id)
+                      send_whatsapp_notification(contact_name, whatsapp_no)
 
 
     def process_contact_group(self):
         contact_id_list = self._df_contact_with_catrgory['contact_id'].tolist()
         contact_id_distinct_list = [*set(contact_id_list)]
         print(contact_id_distinct_list)
-        quit()
         for contact in contact_id_distinct_list:
             df_for_each_contact = self._df_contact_with_catrgory.loc[self._df_contact_with_catrgory['contact_id'] == int(contact)]
             contact_name, email_id, whatsapp_no = self.get_contact_detail(df_for_each_contact)
