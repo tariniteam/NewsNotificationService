4c4983a6124f53c576ab93eb8458ff685494398b
Harsha Navalkar <harsha.navalkar@gmail.com>
2023-05-07 00:05:28 +0530

    GMAIL API Class

--- gmail_api/gmail_api_call.py
+++ gmail_api/gmail_api_call.py
@@ -3,31 +3,33 @@ import ssl
 
 class GmailAPI:
 
-    def __init__(self, ):
+    def __init__(self):
         # Declare port number and server name
-        self.smtp_port = 587   # Standard secure SMTP port
-        self.smtp_server = "smtp.gmail.com"  # Google SMTP Server
-        self.email_from = "tariniteam@gmail.com"
-        self.email_to = "harsha.navalkar@gmail.com"
-        self.pswd = "vrltlgwaasphvjkf"
-        self.message =  "Testing Email!!!"  # content of message
+        self._smtp_port = 587                   # Standard secure SMTP port
+        self._smtp_server = "smtp.gmail.com"    # Google SMTP Server
+        self._email_from = "tariniteam@gmail.com"
+        self._password = "vrltlgwaasphvjkf"
 
-    def send_email (self):
+
+    def send_email (self, message, subject, email_to):
        # Create context
-       self.simple_email_context = ssl.create_default_context()
+       self._subject = subject
+       self._message = message
+       self._email_to = email_to
+       self._simple_email_context = ssl.create_default_context()
        try:
           # Connect to the server
           print("Connecting to server...")
-          TIE_server = smtplib.SMTP(self.smtp_server, self.smtp_port)
-          TIE_server.starttls(context=self.simple_email_context)
-          TIE_server.login(self.email_from, self.pswd)
+          TIE_server = smtplib.SMTP(self._smtp_server, self._smtp_port)
+          TIE_server.starttls(context=self._simple_email_context)
+          TIE_server.login(self._email_from, self._password)
           print("Connected to server :-)")
     
           # Send the actual email
           print()
-          print(f"Sending email to - {self.email_to}")
-          TIE_server.sendmail(self.email_from, self.email_to, self.message)
-          print(f"Email successfully sent to - {self.email_to}")
+          print(f"Sending email to - {self._email_to}")
+          TIE_server.sendmail(self._email_from, self._email_to, self._message)
+          print(f"Email successfully sent to - {self._email_to}")
 
         # If there's an error, print it out
        except Exception as e:
@@ -39,4 +41,4 @@ class GmailAPI:
 
 if __name__ == '__main__':
     objGmailAPI = GmailAPI()
-    objGmailAPI.send_email()
+
